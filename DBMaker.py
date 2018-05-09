a97363df3d4827189dd8c0cc06df39933775c650
Michael Spadafora <michael.spadafora@stonybrook.edu>
2018-05-09 00:59:01 -0400

    test

--- DBMaker.py
+++ DBMaker.py
@@ -8,22 +8,26 @@ db = sqlite3.connect("cards.db")
 c = db.cursor()
 
 # columns = ["description", "plus_actions", "expansion", "plus_treasure", "cost_treasure", "id", "cost_potions", "is_reaction", "name", "is_attack", "trashes", "treasure","plus_cards","victory_points","plus_buys"]
-columns = ["description", "expansion", "cost_treasure", "name"]
-c.execute("CREATE TABLE IF NOT EXISTS cards (name, cost_treasure, description, expansion)")
+columns = ["id" , "description", "expansion", "cost_treasure", "name"]
+c.execute("CREATE TABLE IF NOT EXISTS cards (id, name, cost_treasure, description, expansion)")
 
-# c.execute("select * from cards")
+# c.execute("select  from cards")
 # for card in c.fetchall():
 #     print(card)
-for set in js:
-    for element in js[set]:
-        name = element["name"]
-        scrip = element["description"]
-        expansion = element["expansion"]
-        cost = element["cost_treasure"]
-        card = [name, scrip, expansion, cost]
-        statement = "INSERT INTO cards (name, description, expansion, cost_treasure) values (?,?,?,?)"
-        c.execute(statement, card)
-db.commit()
+i = 0
+# for set in js:
+#
+#     # print('cb' + str(i) + ' = Checkbutton(top,text = "' + set +  '", variable=' + set.lower() + ')')
+#     for element in js[set]:
+#         i = i + 1
+#         name = element["name"]
+#         scrip = element["description"]
+#         expansion = element["expansion"]
+#         cost = element["cost_treasure"]
+#         card = [i, name, scrip, expansion, cost]
+#         statement = "INSERT INTO cards (id, name, description, expansion, cost_treasure) values (?,?,?,?,?)"
+#         c.execute(statement, card)
+# db.commit()
 
 
     # pprint(element["name"])
---
+++ main.py
@@ -0,0 +1,57 @@
+import sqlite3
+from pprint import pprint
+from tkinter import *
+from random import *
+
+
+db = sqlite3.connect("cards.db")
+c = db.cursor()
+
+top = Tk()
+
+promo = IntVar()
+dominion       = IntVar()
+dark_ages      = IntVar()
+prosperity     = IntVar()
+hinterlands    = IntVar()
+cornucopia     = IntVar()
+intrigue       = IntVar()
+seaside        = IntVar()
+alchemy        = IntVar()
+checked = [promo, dominion, dark_ages, hinterlands, prosperity, cornucopia, intrigue, seaside, alchemy]
+
+cb1 = Checkbutton(top,text = "Promo", variable=promo)
+cb2 = Checkbutton(top,text = "Dominion", variable=dominion)
+cb3 = Checkbutton(top,text = "Dark Ages", variable=dark_ages)
+cb4 = Checkbutton(top,text = "Hinterlands", variable=hinterlands)
+cb5 = Checkbutton(top,text = "Prosperity", variable=prosperity)
+cb6 = Checkbutton(top,text = "Cornucopia", variable=cornucopia)
+cb7 = Checkbutton(top,text = "Intrigue", variable=intrigue)
+cb8 = Checkbutton(top,text = "Seaside", variable=seaside)
+cb9 = Checkbutton(top,text = "Alchemy", variable=alchemy)
+
+checkbuttons = [cb1, cb2, cb3, cb4,cb5,cb6,cb7,cb8,cb9]
+
+for butt in checkbuttons:
+    butt.pack()
+var = StringVar();
+var.set("choose items then press generate")
+def generateItems():
+    numChosen = 0
+    while (numChosen < 10):
+        count = c.execute("select count(*) from cards")
+        id = random() * count + 1
+        c.execute("select * from cards where id = ?", id )
+        rows = c.fetchall()
+        noteRows = []
+        for row in rows:
+            if rows.get
+
+
+
+label = Message(top, textvariable = var)
+enter = Button(top, text = "generate", command = generateItems)
+
+enter.pack();
+label.pack();
+top.mainloop()
